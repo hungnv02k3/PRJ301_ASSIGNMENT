0099021970087476e6cf73f531f5b1510454e24e
hungnv02k3 <146454231+hungnv02k3@users.noreply.github.com>
2023-10-06 23:41:47 +0700

    Them 1 tí

---
+++ PRJ_Project/src/java/entity/Student.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author User
+ */
+public class Student extends BaseEntity {
+    private int id;
+    private String name;
+    private Boolean gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+    
+    
+}
---
+++ PRJ_Project/web/veiw/check.jsp
@@ -0,0 +1,29 @@
+<%-- 
+    Document   : check
+    Created on : Oct 6, 2023, 10:23:29 PM
+    Author     : User
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <form id="check" action="check" method="GET">
+            <table border="1px">
+                <tr>
+                <td>NO</td>  
+                <td>ID</td>
+                <td>Name</td>
+                <td>Image</td>
+                <td>Status</td>
+                </tr>
+            </table>
+            
+            
+        </form>
+    </body>
+</html>
